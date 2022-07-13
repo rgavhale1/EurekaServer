7d9d209427e3882d97b67a171368ceca63d760f1
rajgavha <rgavhale@shoppersdrugmart.ca>
2022-07-13 16:54:03 +0530

    test changes

--- src/main/java/com/eureka/server/EurekaServerApplication.java
+++ src/main/java/com/eureka/server/EurekaServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//added changes
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication { 
